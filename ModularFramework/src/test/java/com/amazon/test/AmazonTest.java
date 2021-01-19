9f25aea8e8de3a7a1d8c355d8a2e40fd692b632c
emildam <77644542+emildam@users.noreply.github.com>
2021-01-19 22:23:58 +0000

    Update First Fetch

--- ModularFramework/src/test/java/com/amazon/test/AmazonTest.java
+++ ModularFramework/src/test/java/com/amazon/test/AmazonTest.java
@@ -37,7 +37,7 @@ CommonDriver cmnDriver;
 	
 	@Test
 	public void searchProduct() throws Exception{
-		String product = "Samsung";
+		String product = "Xiomi";
 		String category = "Electronics";
 		
 		homepage.searchProduct(product, category);
